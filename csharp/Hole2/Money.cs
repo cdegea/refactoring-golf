944a4aa4d37a9e485ac940d6c1c1ce09322b72d3
Cristian de Gea <cgea@aidacanarias.com>
2021-11-04 00:41:56 +0100

    Refactored hole 2 and hole 3

---
+++ csharp/Hole2/Money.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hole2
+{
+    public class Money
+    {
+        public int value;
+        public readonly String currency;
+
+        public Money(int value, string currency)
+        {
+            this.value = value;
+            this.currency = currency;
+        }
+    }
+}
