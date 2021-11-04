cf49d339c7d0f3379c88b967acd8f7f4b60af7e9
Cristian de Gea <cgea@aidacanarias.com>
2021-11-04 01:40:09 +0100

    Inline variable return in Apply method

--- csharp/Hole5/TaxRate.cs
+++ csharp/Hole5/TaxRate.cs
@@ -14,8 +14,7 @@ namespace Hole5
         public Money Apply(Money total)
         {
             Double amount = total.value * (this.percent / 100d);
-            Money tax = Money.Create(Convert.ToInt32(amount), total.currency);
-            return tax;
+            return Money.Create(Convert.ToInt32(amount), total.currency);
         }
         public static TaxRate Of(int percent)
         {
